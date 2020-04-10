9501d3c639215099ab27fae355fcb4b4a51e98ad
Rajkumar Natarajan <rajkumar.natarajan@synacor.com>
2020-04-10 02:00:27 -0400

    first commit

--- resource-leak-programmatic-demo/build.gradle.kts
+++ resource-leak-programmatic-demo/build.gradle.kts
@@ -5,5 +5,4 @@ dependencies {
 task("runMain", JavaExec::class) {
     main = "com.myorg.netty.Main"
     classpath = sourceSets["main"].runtimeClasspath
-    jvmArgs = mutableListOf("-Dio.netty.leakDetection.level=PARANOID")
 }
